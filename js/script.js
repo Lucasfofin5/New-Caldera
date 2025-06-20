2580840e86af9f2ec1ce65e7f01f51b28292487d
Lucasfofin5 <kuibre99@gmail.com>
2025-06-20 15:19:58 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+// js/script.js
 
+// Inicializa o estado e a interface
+ui.updateUI();
+// Inicializa os custos exibidos na UI
+ui.updateCosts();
+
+// Configura os botões de ação
+setupActionButtons(); // Chamamos a função do actions.js
+
+// Inicia o loop principal do jogo (ganho de dinheiro por segundo)
+gameLoop.start();
